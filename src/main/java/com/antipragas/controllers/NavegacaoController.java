0ac57caeca682fe2e682fef7a4b118138a8afe9a
Leonardo Puglia <leopuglia.a@gmail.com>
2017-10-07 19:36:27 -0300

    logout com retorno para "/login"

--- src/main/java/com/antipragas/controllers/NavegacaoController.java
+++ src/main/java/com/antipragas/controllers/NavegacaoController.java
@@ -1,11 +1,17 @@
 package com.antipragas.controllers;
 
 import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Thais Camacho
@@ -37,5 +43,14 @@ public class NavegacaoController {
     public String goLogin(){
 
         return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
