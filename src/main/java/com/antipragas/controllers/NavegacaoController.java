6a78ef5f9f0fdee05db9599675678fc0a6a7997e
Leonardo Puglia <leopuglia.a@gmail.com>
2017-10-07 23:03:01 -0300

    logout funcionando em "/usuario/painel"

--- src/main/java/com/antipragas/controllers/NavegacaoController.java
+++ src/main/java/com/antipragas/controllers/NavegacaoController.java
@@ -45,12 +45,4 @@ public class NavegacaoController {
         return "login";
     }
 
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logout (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/";
-    }
 }
